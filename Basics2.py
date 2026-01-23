c75ed2288b8e269677e1d763754b87b2eec874b2
Teju-10 <teju00919@gmail.com>
2026-01-23 11:51:15 +0530

    Updated Basics2.py

--- Basics2.py
+++ Basics2.py
@@ -1,3 +1,6 @@
 # Building the calculator 
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
+
+num3 = int(input("Enter the first number:"))
+num4 = int(input("Enter the second number:"))
